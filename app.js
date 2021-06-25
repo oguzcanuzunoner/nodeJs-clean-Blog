0c4c099b25c66a90c197c33e44cee80b507230fb
Oguzcan Uzunoner <oguzcanuzunoner@icloud.com>
2021-06-25 13:21:35 +0300

    add mongoose and connect mongodb

--- app.js
+++ app.js
@@ -1,36 +1,54 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
+const Photo = require('./models/Photo');
 // const path = require('path')
 const app = express();
 
+mongoose.connect('mongodb://localhost/cleanblog-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
 //TEMPLATE ENGINE
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 
 //MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 
 //ROUTES
-app.get('/',(req,res)=>{
+app.get('/', async (req, res) => {
   //res.sendFile(path.resolve(__dirname,'views/index.html'))
-  res.render('index')
+
+  const photos = await Photo.find({})
+  res.render('index',{
+    photos
+  })
 });
 
-app.get('/about',(req,res)=>{
+app.get('/about', (req, res) => {
   //res.sendFile(path.resolve(__dirname,'views/index.html'))
   res.render('about')
 });
 
 
-app.get('/add_post',(req,res)=>{
+app.get('/add_post', (req, res) => {
   //res.sendFile(path.resolve(__dirname,'views/index.html'))
   res.render('add_post')
+});
+
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body)
+  res.redirect('/')
 });
 
 
 const port = 3000;
 
-app.listen(port,()=>{
+app.listen(port, () => {
   console.log(`Sunucu ${port} portuna bağlandı.`)
 })
