310263a48272ecf5df39483d2c19f5a041cfda6b
Oguzcan Uzunoner <oguzcanuzunoner@icloud.com>
2021-06-28 16:53:46 +0300

    problems

--- app.js
+++ app.js
@@ -1,62 +1,63 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const ejs = require('ejs');
-const Post = require('./models/Post');
+const express = require("express");
+const mongoose = require("mongoose");
+const fileUpload = require("express-fileupload");
+const ejs = require("ejs");
+const Post = require("./models/Post");
 // const path = require('path')
 const app = express();
 
-mongoose.connect('mongodb://localhost/cleanblog-test-db', {
+mongoose.connect("mongodb://localhost/cleanblog-test-db", {
   useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+  useUnifiedTopology: true,
+});
 
 //TEMPLATE ENGINE
-app.set('view engine', 'ejs');
-
+app.set("view engine", "ejs");
 
 //MIDDLEWARES
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(fileUpload());
 
 //ROUTES
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
   //res.sendFile(path.resolve(__dirname,'views/index.html'))
 
-  const posts = await Post.find({})
-  res.render('index', {
-    posts
-  })
+  const posts = await Post.find({});
+  res.render("index", {
+    posts,
+  });
 });
 
-app.get('/about', (req, res) => {
+app.get("/about", (req, res) => {
   //res.sendFile(path.resolve(__dirname,'views/index.html'))
-  res.render('about')
+  res.render("about");
 });
 
-
-app.get('/add_post', (req, res) => {
+app.get("/add_post", (req, res) => {
   //res.sendFile(path.resolve(__dirname,'views/index.html'))
-  res.render('add_post')
+  res.render("add_post");
 });
 
-app.post('/posts', async (req, res) => {
-  await Post.create(req.body)
-  res.redirect('/')
+app.post("/posts", async (req, res) => {
+  console.log(req.files);
+
+  // await Post.create(req.body);
+  // res.redirect("/");
 });
 
-app.get('/posts/:id', async (req, res) => {
-  const post = await Post.findById(req.params.id)
-  res.render('post', { post })
+app.get("/posts/:id", async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  res.render("post", { post });
 });
 
-app.get('/post', (req, res) => {
-  res.render('post')
+app.get("/post", (req, res) => {
+  res.render("post");
 });
 
 const port = 3000;
 
 app.listen(port, () => {
-  console.log(`Sunucu ${port} portuna bağlandı.`)
-})
+  console.log(`Sunucu ${port} portuna bağlandı.`);
+});
